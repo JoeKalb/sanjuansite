909d99e64f0c27fd5c57f77bfe168aa648da5069
JoeKalb <kalb.joseph@gmail.com>
2017-08-14 17:47:35 -0700

    getting ready for posting numbers for twilio authy validation

--- payments.php
+++ payments.php
@@ -1,19 +1,26 @@
 <?php require('config/config.php'); ?>
+<?php 
+	if(isset($_POST['areaCode'])){
+		$areaCode = htmlentities($_POST['areaCode']);
+		$input2 = htmlentities($_POST['input2']);
+		$input3 = htmlentities($_POST['input3']);
+	}
+?>
 <?php include('includes/header.php'); ?>
 <div class="jumbotron">
 	<div class="container">
 		<h3>Billing</h3>
 		<p>This will have something about looking up your bill and having directions on how it will sent a code to confirm if you are the one looking at your bill.</p>
 		<h5>Enter Phone Number: </h5>
-		<form action="">
+		<form method="post" action="payments.php">
 			<div id="phoneNumberInputs">
 				<p>+1</p>
-				<input class="inputs" type="number" placeholder="(949)" maxlength="3" onkeydown="limitText(this, 3);" onkeyup="limitText(this, 3)" id="areaCode">
-				<input class="inputs" type="number" placeholder="555" maxlength="3" onkeydown="limitText(this, 3);" onkeyup="limitText(this, 3)" id="input2">
-				<input class="inputs" type="number" placeholder="5555" maxlength="4" onkeydown="limitText(this, 4);" onkeyup="limitText(this, 4)" id="input3">
+				<input name="areaCode" class="inputs" type="number" placeholder="(949)" maxlength="3" onkeydown="limitText(this, 3);" onkeyup="limitText(this, 3)" id="areaCode">
+				<input name="input2" class="inputs" type="number" placeholder="555" maxlength="3" onkeydown="limitText(this, 3);" onkeyup="limitText(this, 3)" id="input2">
+				<input name="input3" class="inputs" type="number" placeholder="5555" maxlength="4" onkeydown="limitText(this, 4);" onkeyup="limitText(this, 4)" id="input3">
 			</div>
 			<br>
-			<button type="button" class="btn btn-success btn-block" disabled id="submit">Submit</button>
+			<button type="submit" class="btn btn-success btn-block" disabled id="submit">Submit</button>
 		</form>
 	</div>
 </div>
@@ -37,7 +44,7 @@
 	    }
 	});
 
-
+	// check that all values are entered to submit values
 	let button = document.getElementById("submit");
 	let areaCode = false;
 	let input2 = false;
@@ -46,7 +53,6 @@
 		if (areaCode && input2 && input3) button.disabled = false;
 	}
 	
-
 	document.getElementById("areaCode").addEventListener("keyup", function() {
 		if (this.value.length == this.maxLength) {
 			areaCode = true;
@@ -67,6 +73,5 @@
 			changeDisable();
 		}
 	});
-
 </script>
 <?php include('includes/footer.php'); ?>
