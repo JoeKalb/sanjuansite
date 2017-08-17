84cac0a7dc797f231e8613a1e4f7f258316db87f
JoeKalb <kalb.joseph@gmail.com>
2017-08-17 16:58:15 -0700

    started working with bulma

--- owner/dashboard.php
+++ owner/dashboard.php
@@ -1,45 +1,46 @@
 <?php 
 	require('../config/config.php');
 	require('../config/db.php');
-	function getOwner($dbh) {
-		$sql = 'SELECT * FROM owners';
-		foreach ($dbh->query($sql) as $row){
-			
-		}
-	}
-	getOwner($dbh);
+	
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<!--<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.css">
 	<link rel="stylesheet" href="../styles/ownerStyle.css">
 	<title>Owner Dashboard</title>
 </head>
 <body>
-	<div class="wrapper">
+	<h1 class="title is-4">Welcome <?php echo $_COOKIE['firstname']; ?> <?php echo $_COOKIE['lastname']; ?>
+		<a class="button is-danger" href="">LogOut</a>
+	</h1>
+	<div class="columns is-centered">
 		<div class="column">
-			<h3>Receipts</h3>
-			<a href="<?php echo ROOT_URL; ?>owner/addreceipt.php">Create Receipts</a>
-			<table>
+			<h2 class="subtitle">Receipts
+				<a class="button is-primary is-small" href="<?php echo ROOT_URL; ?>owner/addreceipt.php">+</a>
+			</h2>
+			<table class="table">
 				<th>Name</th>
-				<th>Amount Paid</th>
+				<th>Paid</th>
+				<th>Date</th>
 			</table>
 		</div>
 		<div class="column">
-			<h3>Tenents</h3>
-			<a href="<?php echo ROOT_URL; ?>owner/addtenent.php">Create Tenents</a>
-			<table>
+			<h2 class="subtitle">Tenents
+				<a class="button is-primary is-small" href="<?php echo ROOT_URL; ?>owner/addtenent.php">+</a>
+			</h2>
+			<table class="table">
 				<th>Name</th>
 				<th>Monthly Balance</th>
 			</table>
 		</div>
 		<div class="column">
-			<h3>Apartment</h3>
-			<a href="<?php echo ROOT_URL; ?>owner/addapartment.php">Create Apartments</a>
-			<table>
+			<h2 class="subtitle">Apartment
+				<a class="button is-primary is-small" href="<?php echo ROOT_URL; ?>owner/addapartment.php">+</a>
+			</h2>
+			<table class="table">
 				<th>Location</th>
 				<th>Current Rent</th>
 			</table>
--- owner/index.php
+++ owner/index.php
@@ -4,8 +4,30 @@
 	if(isset($_POST['submit'])){
 		$areaCode = htmlentities($_POST['areaCode']);
 		$number = htmlentities($_POST['number']);
-		echo $areaCode;
-		echo $number;
+		$sql = "SELECT * FROM owners WHERE areacode={$areaCode} AND phonenumber={$number}";
+		$user = findOwner($dbh, $sql);
+		if(is_null($user)){
+			echo 'User not found';
+		}else{
+			goToDash($user);
+		}
+	}
+
+	function findOwner($conn, $queryValues){
+		foreach ($conn->query($queryValues) as $user){
+			return $user;
+		}
+	}
+	function goToDash($owner){
+		$time = time()+360000;
+		setcookie('id', $owner['id'], $time);
+		setcookie('firstname', $owner['firstname'], $time);
+		setcookie('lastname', $owner['lastname'], $time);
+		setcookie('areacode', $owner['areacode'], $time);
+		setcookie('phonenumber', $owner['phonenumber'], $time);
+		setcookie('email', $owner['email'], $time);
+
+		header('Location: dashboard.php');
 	}
 ?>
 <!DOCTYPE html>
