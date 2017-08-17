34f59ee412a157e403bfd29727fab44a9dfdf580
JoeKalb <kalb.joseph@gmail.com>
2017-08-16 19:26:35 -0700

    css issue w/ livereload, getting forms ready

--- owner/dashboard.php
+++ owner/dashboard.php
@@ -4,12 +4,7 @@
 	function getOwner($dbh) {
 		$sql = 'SELECT * FROM owners';
 		foreach ($dbh->query($sql) as $row){
-			print $row['id'] . "\t";
-			print $row['firstname'] . "\t";
-			print $row['lastname'] . "\t";
-			print $row['areacode'] . "\t";
-			print $row['phonenumber'] . "\t";
-			print $row['email'] . "\n";
+			
 		}
 	}
 	getOwner($dbh);
@@ -18,20 +13,37 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<!--<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../styles/ownerStyle.css">
 	<title>Owner Dashboard</title>
 </head>
 <body>
-	<div>
-		<h3>Receipts</h3>
-		<button>Create Receipts</button>
-	</div>
-	<div>
-		<h3>Tenents</h3>
-		<button>Create Tenents</button>
-	</div>
-	<div>
-		<h3>Apartment</h3>
-		<button>Create Apartments</button>
+	<div class="wrapper">
+		<div class="column">
+			<h3>Receipts</h3>
+			<a href="<?php echo ROOT_URL; ?>owner/addreceipt.php">Create Receipts</a>
+			<table>
+				<th>Name</th>
+				<th>Amount Paid</th>
+			</table>
+		</div>
+		<div class="column">
+			<h3>Tenents</h3>
+			<a href="<?php echo ROOT_URL; ?>owner/addtenent.php">Create Tenents</a>
+			<table>
+				<th>Name</th>
+				<th>Monthly Balance</th>
+			</table>
+		</div>
+		<div class="column">
+			<h3>Apartment</h3>
+			<a href="<?php echo ROOT_URL; ?>owner/addapartment.php">Create Apartments</a>
+			<table>
+				<th>Location</th>
+				<th>Current Rent</th>
+			</table>
+		</div>
 	</div>
 </body>
 </html>
